0cad11618e086d373fd4f240ee32e06a4666762e
Jhonatan <jhonatan-ito@hotmail.com>
2026-02-04 13:25:46 +0900

    google auth fixed

--- backend/routes/googleRoute.js
+++ backend/routes/googleRoute.js
@@ -17,8 +17,6 @@ googleRoute.get(
   (req, res) => {
     const { token } = req.user;
 
-    res.redirect(
-      `https://pomoxp-production.up.railway.app/login/success?token=${token}`,
-    );
+    res.redirect(`https://pomo-xp.vercel.app//login/success?token=${token}`);
   },
 );
